1ed5bee9aba8eb148d6419aa2b10acb46f4580f5
Alfonso de la Guarda Reyes <alfonsodg@gmail.com>
2016-08-19 13:08:48 -0500

    Added customers for sales

--- controllers/customers.py
+++ controllers/customers.py
@@ -52,6 +52,102 @@ def manage():
                              csv=False)
     return dict(form=form, title=title)
 
+@auth.requires_membership('sales')
+def manage_limited():
+    user_id = auth.user.id
+    sales_user_id = 1
+    sales_user = auth.has_membership(role='sales')
+    root_user = auth.has_membership(role='root')
+    if sales_user:
+        sales_user_id = db.sellers(db.sellers.related_user == user_id).id
+        query = ((db.customers.id > 0) & (db.customers_sellers.seller == sales_user_id))
+    else:
+        query = ((db.customers.id > 0))
+    print query
+    response.view = 'default.html'
+    title = T('Customer Management')
+    db.services_configurations.customer_service.writable = False
+    # form = SQLFORM.smartgrid(db.customers,
+    #                          linked_tables=['customers_addresses', 'customers_contacts',
+    #                                         'customers_contacts_phones', 'customers_services',
+    #                                         'customers_rates', 'customers_contracts',
+    #                                         'customers_contracts_documents',
+    #                                         'services_configurations'],
+    #                          fields=[db.customers.name,
+    #                                  db.customers.legal_name,db.customers.tax_id,
+    #                                  db.customers.activity_start,db.customers.account_type,
+    #                                  db.customers.currency,db.customers.commercial_document,
+    #                                  db.customers.tax, db.customers.status,
+    #                                  db.customers_addresses.name, db.customers_addresses.address,
+    #                                  db.customers_addresses.reference, db.customers_addresses.country,
+    #                                  db.customers_addresses.tax_address,
+    #                                  db.customers_contacts.area, db.customers_contacts.role,
+    #                                  db.customers_contacts.document_id, db.customers_contacts.name,
+    #                                  db.customers_contacts.email,
+    #                                  db.customers_contacts_phones.name, db.customers_contacts_phones.phone,
+    #                                  db.customers_contacts_phones.extension, db.customers_contacts_phones.main,
+    #                                  db.customers_contracts.name, db.customers_contracts.start_date,
+    #                                  db.customers_contracts.end_date, db.customers_contracts.archive,
+    #                                  db.customers_contracts_documents.name, db.customers_contracts_documents.archive,
+    #                                  db.customers_rates.package, db.customers_rates.name,
+    #                                  db.customers_rates.rate,
+    #                                  db.customers_services.service, db.customers_services.seller,
+    #                                  db.customers_services.billing_period,
+    #                                  db.customers_services.currency, db.customers_services.did,
+    #                                  db.customers_services.account, db.customers_services.password,
+    #                                  db.services_configurations.provider, db.services_configurations.billing_period,
+    #                                  db.services_configurations.currency, db.services_configurations.rate_cost
+    #                                  ],
+    #                          left=db.customers_sellers.on(db.customers_sellers.customer==db.customers.id),
+    #                          deletable=False,
+    #                          editable=False,
+    #                          create=False,
+    #                          constraints=dict(customers_sellers=query),
+    #                          #buttons_placement='left',
+    #                          #links_placement='left',
+    #                          csv=False)
+    form = SQLFORM.grid(query,
+                             # linked_tables=['customers_addresses', 'customers_contacts',
+                             #                'customers_contacts_phones', 'customers_services',
+                             #                'customers_rates', 'customers_contracts',
+                             #                'customers_contracts_documents',
+                             #                'services_configurations'],
+                             fields=[db.customers.name,
+                                     db.customers.legal_name,db.customers.tax_id,
+                                     db.customers.activity_start,db.customers.account_type,
+                                     db.customers.currency,db.customers.commercial_document,
+                                     db.customers.tax, db.customers.status,
+                                     # db.customers_addresses.name, db.customers_addresses.address,
+                                     # db.customers_addresses.reference, db.customers_addresses.country,
+                                     # db.customers_addresses.tax_address,
+                                     # db.customers_contacts.area, db.customers_contacts.role,
+                                     # db.customers_contacts.document_id, db.customers_contacts.name,
+                                     # db.customers_contacts.email,
+                                     # db.customers_contacts_phones.name, db.customers_contacts_phones.phone,
+                                     # db.customers_contacts_phones.extension, db.customers_contacts_phones.main,
+                                     # db.customers_contracts.name, db.customers_contracts.start_date,
+                                     # db.customers_contracts.end_date, db.customers_contracts.archive,
+                                     # db.customers_contracts_documents.name, db.customers_contracts_documents.archive,
+                                     # db.customers_rates.package, db.customers_rates.name,
+                                     # db.customers_rates.rate,
+                                     # db.customers_services.service, db.customers_services.seller,
+                                     # db.customers_services.billing_period,
+                                     # db.customers_services.currency, db.customers_services.did,
+                                     # db.customers_services.account, db.customers_services.password,
+                                     # db.services_configurations.provider, db.services_configurations.billing_period,
+                                     # db.services_configurations.currency, db.services_configurations.rate_cost
+                                     ],
+                             left=db.customers_sellers.on(db.customers_sellers.customer==db.customers.id),
+                             deletable=False,
+                             editable=False,
+                             create=False,
+                             # constraints=dict(customers_sellers=query),
+                             #buttons_placement='left',
+                             #buttons_placement='left',
+                             #links_placement='left',
+                             csv=False)
+    return dict(form=form, title=title)
+
 
 @auth.requires_membership('root')
 def contract():
